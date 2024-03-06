a5816f817f16ff2f308e9dde5f9cce55eae35671
Kesireddi Nirbhai Sai <kesireddi.nirbhaisai@gmail.com>
2024-03-06 14:01:20 +0530

    initial commit

---
+++ src/com/targetready/assignments/Calender.java
@@ -0,0 +1,7 @@
+package com.targetready.assignments;
+public class Calender {
+    public static void main(String[] args) {
+        System.out.println("Hello! World");
+        System.out.println("hi");
+    }
+}
